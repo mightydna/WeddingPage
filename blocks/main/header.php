423aec255586c3ec4bb3183b5ed90bd9abd89941
Dominik <dominik.nagel@stormchaser-games.com>
2026-01-05 20:48:34 +0100

    First basic tool setup

---
+++ blocks/main/header.php
@@ -0,0 +1,53 @@
+<header id="header">
+    <div id="logo_block">
+        <img src="resources/icons/logo_turquoise.svg" alt="Website Icon" id="header_logo">
+        <p id="logo"><a href="#top">Unsere Hochzeit</a></p>
+    </div>
+    <nav id="menu">
+        <ul>
+            <li><div class="menu_box" id="menu_btn_about"><a href="#about">About</a></div></li>
+            <li><div class="menu_box" id="menu_btn_location"><a href="#location">Location</a></div></li>
+            <li><div class="menu_box" id="menu_btn_travel"><a href="#travel">Anreise</a></div></li>
+            <li><div class="menu_box" id="menu_btn_food"><a href="#food">Essen</a></div></li>
+            <li><div class="menu_box" id="menu_btn_form"><a href="#form">Zu- und Absage</a></div></li>
+            <li id="lang_select_container"><div class="menu_box" id="lang_btn_de">DE</div><div class="menu_box">|</div><div class="menu_box" id="lang_btn_en">EN</div></li>
+        </ul>
+    </nav>
+    <!-- Burgermenu -->
+    <nav id="burgermenu">
+        <div id="burgermenu_main">
+            <img src="resources/icons/burger-menu-icon.svg" id="header_burger_icon" alt="Burgermenü-Icon">
+        </div>
+    </nav>
+    <div id="burgermenu_overlay" >
+        <div id="bmenu_overlay_close_container">
+            <img src="resources/icons/icons8-löschen.svg" id="bmenu_overlay_close_btn" alt="Schliessen-Icon">
+        </div>
+        <div id="logo_block_overlay">
+            <img src="resources/icons/logo_turquoise.svg" alt="Website Icon" id="header_logo_overlay">
+            <p id="logo_overlay"><a href="#top">DOMINIK</a></p>
+        </div>
+        <div id="burgermenu_list_container">
+            <p id="burgermenu_title">Wo möchtest du hin?</p>
+            <ul id="burgermenu_list">
+                <li><div class="burger_menu_box" id="burger_menu_btn_about"><a href="#about">About</a></div></li>
+                <li><div class="burger_menu_box" id="burger_menu_btn_location"><a href="#location">Location</a></div></li>
+                <li><div class="burger_menu_box" id="burger_menu_btn_travel"><a href="#travel">Anreise</a></div></li>
+                <li><div class="burger_menu_box" id="burger_menu_btn_food"><a href="#food">Essen</a></div></li>
+                <li><div class="burger_menu_box" id="burger_menu_btn_form"><a href="#form">Zu- und Absage</a></div></li>
+            </ul>
+        </div>
+        <div id="bmenu_language_menu">
+            <p id="bmenu_language_menu_title">Sprache ändern?</p>
+            <div id="bmenu_language_menu_select">
+                <p id="overlay_lang_btn_de">DE</p>
+                <p>|</p>
+                <p id="overlay_lang_btn_en">EN</p>
+            </div>
+        </div>
+        <div id="bmenu_b2t_container">
+            <div id="bmenu_b2t_bar"></div>
+            <div id="bmenu_b2t"><a href="#top">Zurück zum Anfang?</a></div>
+        </div>
+    </div>
+</header>
---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet/less" href="resources/styles/base/all.less">
+    <script>less = {env: "development"};</script>
+    <script src="resources/vendors/less.js-4.4.2/dist/less.min.js"></script>
+
+    <title>Unsere Hochzeit</title>
+</head>
+<body>
+    <?php
+    require '../blocks/main/header.php';
+    ?>
+</body>
+</html>
